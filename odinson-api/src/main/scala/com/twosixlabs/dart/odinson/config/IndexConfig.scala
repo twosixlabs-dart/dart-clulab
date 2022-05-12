873c9a38bbac9af7b5ff7ada6d1870fb992c7f22
Michael Reynolds <michael.reynolds@twosixlabs.com>
2022-05-12 12:58:48 -0400

    [DART-1418] - update odinson to 0.6.1 in order to get incremental indexing (#2)

--- odinson-api/src/main/scala/com/twosixlabs/dart/odinson/config/IndexConfig.scala
+++ odinson-api/src/main/scala/com/twosixlabs/dart/odinson/config/IndexConfig.scala
@@ -8,13 +8,19 @@ object IndexConfig {
         val indexDir : String = config.getString( "dir" )
         val maxTokens : Int = config.getInt( "maxNumberOfTokensPerSentence" )
         val invalidCharReplacement : String = config.getString( "invalidCharacterReplacement" )
+        val incremental = config.getBoolean( "incremental" )
+        val refreshMs = config.getInt( "refreshMs" )
 
         IndexConfig( dir = indexDir,
                      maxNumberOfTokensPerSentence = maxTokens,
-                     invalidCharacterReplacement = invalidCharReplacement )
+                     invalidCharacterReplacement = invalidCharReplacement,
+                     incremental = incremental,
+                     refreshMs = refreshMs )
     }
 }
 
 case class IndexConfig( dir : String,
                         maxNumberOfTokensPerSentence : Int,
-                        invalidCharacterReplacement : String )
+                        invalidCharacterReplacement : String,
+                        incremental : Boolean,
+                        refreshMs : Int )
